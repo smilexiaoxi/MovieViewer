1b9c9f4a7957dfbb5c91cefa03b9adfd33b44432
Xiaoxi Zhang <xiaoxizhang@xiaoxis-macbook-pro.local>
2016-02-02 20:29:55 -0500

    Initial Commit

---
+++ MovieViewer/MovieCell.swift
@@ -0,0 +1,31 @@
+//
+//  MovieCell.swift
+//  MovieViewer
+//
+//  Created by Xiaoxi Zhang on 1/27/16.
+//  Copyright © 2016 Xiaoxi Zhang. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
